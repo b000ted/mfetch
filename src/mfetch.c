28c41c2736881a8191f038e690572e81ca8e09bf
b000ted <b000ted@proton.me>
2026-09-10 13:34:16 -0700

    edit

--- src/mfetch.c
+++ src/mfetch.c
@@ -19,13 +19,15 @@
 
 /* Helpers */
 
+#ifndef __APPLE__
 static void
 chomp(char *s)
 {
-	size_t n = strlen(s);
-	while (n > 0 && (s[n-1] == '\n' || s[n-1] == '\r' || s[n-1] == ' '))
-		s[--n] = '\0';
+    size_t n = strlen(s);
+    while (n > 0 && (s[n-1] == '\n' || s[n-1] == '\r' || s[n-1] == ' '))
+        s[--n] = '\0';
 }
+#endif
 
 /* OS/kernel */
 
